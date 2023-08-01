956f301bd0d979f1ec5faeb92b0521ccb4101649
nikmomo <zhenz@vt.edu>
2023-08-01 02:26:59 -0400

    generate a unittest project

---
+++ riichi_display_test/PlayerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace riichi_display_test
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
